83fed25e86669e82af4513664c5c212dec94b4cb
Trieu Viet Hung <69461968+hungtrieu07@users.noreply.github.com>
2023-10-03 09:38:30 +0700

    create db with defined collections

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,3 @@ COPY init_mongo.js /docker-entrypoint-initdb.d/
 
 # Expose the MongoDB default port (27017)
 EXPOSE 27017
-
-# Define environment variables for MongoDB
-ENV MONGO_INITDB_ROOT_USERNAME=root
-ENV MONGO_INITDB_ROOT_PASSWORD=nguyen
--- init_mongo.js
+++ init_mongo.js
@@ -1,18 +1,6 @@
 // create a new database named testdb
 db = db.getSiblingDB("vehicles_db");
 
-// create a new user for the testdb database
-db.createUser({
-  user: "root",
-  pwd: "nguyen",
-  roles: [
-    {
-      role: "readWrite",
-      db: "vehicles_db",
-    },
-  ],
-});
-
 // create a new collection named users in the testdb database
 db.createCollection("users");
 db.createCollection("vehicles");
