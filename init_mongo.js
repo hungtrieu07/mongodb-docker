2d945de326815e250ea94a579bfb08852a19265a
Trieu Viet Hung <69461968+hungtrieu07@users.noreply.github.com>
2023-10-11 16:09:06 +0700

    add auto restart when docker crash

--- init_mongo.js
+++ init_mongo.js
@@ -1,15 +1,4 @@
-// create a new database named testdb
-db = db.getSiblingDB("vehicles_db");
-
-// create a new collection named users in the testdb database
-db.createCollection("users");
-db.createCollection("vehicles");
-db.createCollection("violation_vehicles");
-
-// insert some documents into the users collection
-// db.users.insertMany([
-//   { name: "Alice", age: 25 },
-//   { name: "Bob", age: 30 },
-//   { name: "Charlie", age: 35 },
-// ]);
+db = db.getSiblingDB("LPR");
+db.createCollection("queue");
+db.createCollection("requested");
 
